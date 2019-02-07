0e3357d4c9b0c182652dcc5f5c463f25e53927f2
Morgane Figard <morgane.figard@edu.ece.fr>
2019-02-08 00:19:36 +0100

    Change profile card layout

--- page_facebook/src/components/Personne.js
+++ page_facebook/src/components/Personne.js
@@ -6,9 +6,11 @@ export default class Personne extends Component {
     return (
       <div className="Personne" style={{backgroundColor: this.props.pers.bg_col}}>
         <img className="ProfileImg" src={this.props.pers.image} alt="Avatar"/>
-        <p>{this.props.pers.prenom} {this.props.pers.nom}</p>
-        <p>{this.props.pers.date_naissance}</p>
-        <button onClick={() => this.props.changeBackgroundColor(this.props.pers)}>Changer la couleur de fond</button>
+        <h2>{this.props.pers.prenom} {this.props.pers.nom}</h2>
+        <p>Né(e) le <b>{this.props.pers.date_naissance}</b></p>
+        <p className="BgButtonContainer">
+          <button className="BgButton" onClick={() => this.props.changeBackgroundColor(this.props.pers)} >Changer la couleur de fond</button>
+        </p>
       </div>
     )
   }
